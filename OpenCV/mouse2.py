7cd110f3fab65a50fabeff8a89637f914be7be51
Diabohallu <obaidullahbinahmed2007@gmail.com>
2026-07-10 14:35:21 +0400

    Modify circle thickness in mouse click event
    Changed circle thickness from default to 4 in mouse click event.

--- OpenCV/mouse2.py
+++ OpenCV/mouse2.py
@@ -5,7 +5,7 @@ import numpy as np
 image = np.zeros((500, 500, 3), dtype = "uint8")
 def mouse_click(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(image, (x,y), 40, (0, 0, 255), -1)
+        cv2.circle(image, (x,y), 40, (0, 0, 255), 4)
     elif event == cv2.EVENT_RBUTTONDOWN:
         cv2.rectangle(image, (x-40, y-20), (x+40, y+20), (0, 255, 0), -1)
 cv2.namedWindow("Canvas")
