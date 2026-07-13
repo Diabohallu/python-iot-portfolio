2c9f223e617e2a223470cf9dd075e4cab2ef6e1a
Diabohallu <obaidullahbinahmed2007@gmail.com>
2026-07-13 22:00:20 +0400

    Refactor clap detection logic and fix variable names

--- Arduino/analog_sound_sensor/analog_sound_sensor.ino
+++ Arduino/analog_sound_sensor/analog_sound_sensor.ino
@@ -1,93 +1,22 @@
-// Stable Clap Switch using Analog Sound Sensor
-
-const int soundPin = A0; //Analog output
-const int relayPin = 8; //Relay Control Pin 
-
+const int soundPin = A0; 
+const int relayPin = 8; 
 int threshold = 300;
 bool bulbState = false;
 unsigned long lastClapTime = 0;
-const unsigned long debouncedelay = 700; //ms
+const unsigned long debounceDelay = 700; 
 
 void setup() {
-
-  pinMode(relaypin,OUTPUT);
-  digitalWrite(relaypin,LOW); //Bulb Off initially
-
+  pinMode(relayPin, OUTPUT); 
+  
+  digitalWrite(relayPin, LOW); 
 }
 
 void loop() {
-
   int soundValue = analogRead(soundPin);
 
-  //Detected loud clap
-  if (soundValue > threshold && (millis()-lastClapTime)>debounceDelay) {
-    bulbState = !bulbState; //Toggle bulb
-    digitalWrite(relayPin, bulbState); //Control Relay
-    lastclapTime = millis();
+  if (soundValue > threshold && (millis() - lastClapTime) > debounceDelay) {
+    bulbState = !bulbState; 
+    digitalWrite(relayPin, bulbState ? HIGH : LOW); 
+    lastClapTime = millis(); 
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
